0b62058c95db395c74c2d7bf59c96d5e34f361ed
Ram Maheshwari <ram991011@gmail.com>
2020-11-28 19:24:12 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 import './App.scss'
 import Home from './pages/Home'
 
@@ -6,8 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <nav></nav>
-        <Home />
+        <nav>
+          <div className="container">
+            <ul className="links">
+              <Link to="/">Home</Link>
+              <Link to="/">Chart</Link>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact={true} component={Home} />
         <footer>
           <div className="container">
             <h4 className="text">
